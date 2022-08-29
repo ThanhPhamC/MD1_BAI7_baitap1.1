5629d9e10d159d12248a925850a5c0eb35cf85c6
Phạm Thanh <thanhphamhumg@gmail.com>
2022-08-29 20:59:07 +0700

    lele

---
+++ sdsd.js
@@ -0,0 +1,11 @@
+
+let vl=prompt('Nhập điểm Vật Lý');
+let hh=prompt('Nhập điểm Hoá Học');
+let sh=prompt('Nhập điểm Sinh Học');
+let a = parseInt(vl);
+let b = parseInt(hh);
+let c = parseInt(sh);
+let d=a+b+c;
+let e=d/3;
+
+document.write('Điểm trung bình là '+e);
